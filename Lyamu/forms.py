9f5afb15a48ad68f0288731d0244d07f0b72efa7
devis <devisarineitwe2000@gmail.com>
2024-01-08 15:53:13 +0300

    8th jan 15pm

--- Lyamu/forms.py
+++ Lyamu/forms.py
@@ -6,7 +6,7 @@ from Lyamu.models import Job
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        fields = ['title', 'description', 'requirements', 'importance_level', 'application_deadline']  # Add more fields as needed
+        fields = ['employer', 'title', 'description', 'requirements', 'importance_level', 'application_deadline', 'is_active']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
